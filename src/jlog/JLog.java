c64a80a7412a2796ea18ded63c6945057ed729e4
Yasyf Mohamedali <yasyf@yasyf.com>
2012-10-17 23:28:22 -0700

    Added relevant comments to explain process from calculator's POV

--- src/jlog/JLog.java
+++ src/jlog/JLog.java
@@ -8,16 +8,21 @@ import javax.swing.JOptionPane;
  * @author yasyf
  */
 public class JLog {
-
+//static variables
     public static Double base;
     public static Double x;
+    
     // 0 = log
     // 1 = ln
     public static int function;
 
     public static void main(String[] args) {
+       //JOptionPane lines are merely for functional GUI purposes        
         String[] options = new String[]{"log", "ln"};
         function = JOptionPane.showOptionDialog(null, "Which Function?", "JLog", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
+        
+        //calculator runs the following on button press
+        //determine what variation of function was intended
         if (function == 0) {
 
             try {
@@ -37,7 +42,7 @@ public class JLog {
             System.exit(1);
         }
 
-
+        //calculate the answer, depending on what function was intended
         if (base != null) {
 
             if (base == Math.E) {
@@ -55,7 +60,7 @@ public class JLog {
     }
 
     public static double c_log(Double x, Double base) {
-        //This is the function your calculator runs
+        //This is the function your calculator runs to actually calculate the log behind the scenes
         if (base != null) {
             return Math.log(x) / Math.log(base);
         } else {
@@ -66,6 +71,7 @@ public class JLog {
     }
 
     public static void result(String message) {
+        //display GUI prompt of result
         JOptionPane.showMessageDialog(new JDialog(), message + " = " + c_log(x, base), message, JOptionPane.INFORMATION_MESSAGE);
 
     }
